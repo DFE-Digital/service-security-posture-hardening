923130af5f42676e7e0bfd018624812fbbfaf70b
Ian Pearl <50660922+casetone@users.noreply.github.com>
2023-05-03 16:06:20 +0100

    it

--- acs.py
+++ acs.py
@@ -32,7 +32,7 @@ class SplunkACS:
         print(f"gonna install {app_path}")
         print(self.all_headers)
         
-        files = {"package": open(app_path,"rb"), "token": "eyJraWQiOiJCSG1zbWdSdDFRVjNjRE1aLWhOTWZmdkJIQ3FUbDNhTmZYMWxqa2U5QnRBIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmNQVE9QcTF4bml0T2ZrTE95TFQtN3F5TXVyU21zNldrYVByY2JtR3llZFkiLCJpc3MiOiJodHRwczovL2lkcC5sb2dpbi5zcGx1bmsuY29tL29hdXRoMi9hdXNnemp3c2V0Y2hQVklsZzJwNyIsImF1ZCI6ImFwaS5zcGx1bmsuY29tIiwiaWF0IjoxNjgzMTI1Nzk5LCJleHAiOjE2ODMxNTQ1OTksImNpZCI6IjBvYWd6anZwcXRzZHRZUDM1MnA3IiwidWlkIjoiMDB1ZXA4aHlncEpaZ2FMUEwycDciLCJzY3AiOlsib3BlbmlkIl0sImF1dGhfdGltZSI6MTY4MzEyNTc5OSwic3ViIjoiaWFuX3BlYXJsIiwidW5hbWUiOiJpYW5fcGVhcmwiLCJuYW1lIjoiSWFuIFBlYXJsIiwidXNlciI6eyJlbWFpbCI6ImlhbkBwM2FybC5jb20ifSwiZW1haWwiOiJpYW5AcDNhcmwuY29tIn0.JfeS7DP9EDnkztZp0Is5-aCfBHDIM3xBWpM447DtR0LIr3OuPjxdzkRtb3nHq-M23kzG6XAc536z9nEBdzuCpRTpQr-qNbNT3RBm57-Hy8u9q8CY_bc0cZNXax-_6QZrmBJW7WbltXcKQHKy2TMZnOLY7MR6z95CTCWTO8wb7M-akri_geeDyklf1OeSBgPlSZYDfqYiISoTaAfyMzMVK0wCWQMmTPmbAqUX2Jw2PWh6mpehAuWbIev1iMvw0ldRnn2i5RS69SYR-eQ292e-pMwoLaGHfGKLjpZMWWhDAU3-4lupsDimEGX1zzPxtnTy8fK-dfQJNQrO39xfzTzRug"}
+        files = {"package": open(app_path,"rb"), "token": "token"}
         response = requests.post(self.url, headers=self.all_headers, files = files)
 
         return response
