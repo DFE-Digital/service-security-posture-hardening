d80d5c56d0aeffbf57366d8237aba7d69ddc50cd
Ian Pearl <50660922+casetone@users.noreply.github.com>
2023-07-11 10:39:04 +0100

    create new metrics app

---
+++ SSPHP/SSPHP_metrics/appserver/static/js/addtags.js
@@ -0,0 +1,42 @@
+let change = (mutationList, observer) => {
+
+    console.log("mutation detected");
+
+    for (var mutationListIter = 0; mutationListIter < mutationList.length; mutationListIter++) {
+
+        let tds = mutationList[mutationListIter].target.getElementsByTagName("TD");
+
+        for (var tdsIter = 0; tdsIter < tds.length; tdsIter++) {
+
+            if (tds[tdsIter].classList.contains("string") && !tds[tdsIter].classList.contains("ssphp_modified")) {
+
+                console.log("mutatating string");
+
+                tds[tdsIter].innerHTML = tds[tdsIter].innerHTML.replace(/~~(.*?)~~(.*?)~~/g, '<span class="$2">$1</span>');
+
+                tds[tdsIter].innerHTML = tds[tdsIter].innerHTML.replace(/~!(.*?)~!(.*?)~!(.*?)~!/g, '<$1>$2<$3>');
+
+                tds[tdsIter].classList.add("ssphp_modified");
+
+            }
+        }
+
+    }
+
+};
+
+console.log("Starting observations");
+
+const observer = new MutationObserver(change);
+const config = { attributes: true, childList: true, subtree: true };
+
+let targetNodes = document.getElementsByClassName("dashboard-element table");
+
+for (var i = 0; i < targetNodes.length; i++) {
+
+    console.log(`observering: ${targetNodes[i]}`);
+
+    observer.observe(targetNodes[i], config);
+}
+
+console.log("Observations complete");
