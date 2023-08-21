79c8193b36db6611a0fc541f5944cf7a61a3d1bc
Alex Kinnane <17098249+akinnane@users.noreply.github.co>
2023-08-21 10:50:54 +0100

    Add terraform state

---
+++ data_ingester/terraform/state_terraform/terraform_state.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate"
+  location = "West Eurpoe"
+  tags = {
+    "Product"     = ""
+    "Environment" = "Dev"
+
+  }
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                            = "tfstate${random_string.resource_code.result}"
+  resource_group_name             = azurerm_resource_group.tfstate.name
+  location                        = azurerm_resource_group.tfstate.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
