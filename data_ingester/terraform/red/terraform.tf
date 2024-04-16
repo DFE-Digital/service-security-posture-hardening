a655e98517fa2c78b34e097190a9b5beb1bf617a
pritchyspritch <s.pritch@protonmail.com>
2024-04-16 12:33:58 +0100

    Add me to allow list

--- data_ingester/terraform/red/terraform.tf
+++ data_ingester/terraform/red/terraform.tf
@@ -25,7 +25,7 @@ locals {
   #  sku_name_python      = "Y1"
   sku_name_rust        = "EP1"
   key_vault_name       = "SSPHP-Metrics"
-  key_vault_object_ids = ["393279ef-dc89-4bff-8186-4d283ee7b280", "7e53a6ad-8c94-4d82-a3b5-222882dffc29"]
+  key_vault_object_ids = ["393279ef-dc89-4bff-8186-4d283ee7b280", "7e53a6ad-8c94-4d82-a3b5-222882dffc29", "0870054a-44a7-4ddf-af8d-e48276f81df4"] # 7e53 = GitHub Actions - TODO; check if still required
 }
 
 provider "azurerm" {
