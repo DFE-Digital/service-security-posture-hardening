34f48c820ce611e2e3cc758e0cb696dfb6164456
pritchyspritch <47423802+pritchyspritch@users.noreply.github.com>
2024-09-16 16:29:54 +0100

    Change cloudtrail test

--- data_ingester/terraform/infrastructure_for_test/aws/terraform.tf
+++ data_ingester/terraform/infrastructure_for_test/aws/terraform.tf
@@ -133,6 +133,75 @@ resource "aws_iam_server_certificate" "test_cert" {
   private_key      = file("key.pem")
 }
 
+resource "aws_cloudtrail" "test-cloudtrail" {
+  depends_on = [aws_s3_bucket_policy.s3_bucket_policy_test]
+
+  name                          = "cloudtrail_test"
+  s3_bucket_name                = aws_s3_bucket.s3_bucket_cloudtrail_test.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+}
+
+resource "aws_s3_bucket" "s3_bucket_cloudtrail_test" {
+  bucket        = "tf-test-s3-trail-dcap"
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "aws_iam_policy_doc_s3" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.s3_bucket_cloudtrail_test.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/cloudtrail_test"]
+    }
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.s3_bucket_cloudtrail_test.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/cloudtrail_test"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy_test" {
+  bucket = aws_s3_bucket.s3_bucket_cloudtrail_test.id
+  policy = data.aws_iam_policy_document.aws_iam_policy_doc_s3.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
+
 # resource "aws_route53_zone" "primary" {
 #   name = "example.com"
 # }
