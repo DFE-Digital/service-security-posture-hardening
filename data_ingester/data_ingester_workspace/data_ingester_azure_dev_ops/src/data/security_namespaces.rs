fae2cf4b9fd98126bd449d88082ebe9a1d4f3647
Alex Kinnane <17098249+akinnane@users.noreply.github.com>
2025-03-25 15:04:33 +0000

    wip

---
+++ data_ingester/data_ingester_workspace/data_ingester_azure_dev_ops/src/data/security_namespaces.rs
@@ -0,0 +1,46 @@
+use serde::Deserialize;
+use serde::Serialize;
+use tracing::error;
+
+use crate::ado_response::AdoResponse;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SecurityNamespaces {
+    pub namespaces: Vec<SecurityNamespace>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SecurityNamespace {
+    // pub collection: Collection,
+    // pub default_team: DefaultTeam,
+    pub namespace_id: String,
+    pub name: String,    
+    // pub last_update_time: String,
+    // pub name: String,
+    // pub revision: i64,
+    // pub state: String,
+    // pub url: String,
+    // pub visibility: String,
+}
+
+
+impl From<AdoResponse> for SecurityNamespaces {
+    fn from(value: AdoResponse) -> Self {
+        let collection = value.value.into_iter().filter_map(|repo| {
+            match serde_json::from_value::<SecurityNamespace>(repo) {
+                Ok(repo) => {
+                    Some(repo)
+                },
+                Err(err) => {
+                    error!(name="Azure DevOps", operation="From<AdoResponse> for Repositories", error=?err);
+                    None
+                }
+            }
+        }).collect();
+        Self {
+            namespaces:collection,
+        }
+    }
+}
